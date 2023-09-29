a22679c651435a544ceb9ffb5ad03917aafbcb83
gk1706 <121346044+Koundu@users.noreply.github.com>
2023-09-29 12:03:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const form = document.getElementById("form1")
+const submit = form[2];
+
 function getFormvalue() {
     //Write your code here
-
+	fName = form[0].value;
+	lName = form[1].value;
+	alert(fName +" "+lName);
 }
+
+submit.addEventListener('click',()=>{
+	getFormvalue(fName,lName);
+})
